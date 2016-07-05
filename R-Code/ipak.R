735ec8690948f6457dd6f41fab2d9eed3c70a1f0
Simeone Marino <simeonem@gmail.com>
2016-07-05 14:53:49 -0400

    Function that groups together all the packages needed,  checks if they are installed, installs them if needed and loads them.

---
+++ R-Code/ipak.R
@@ -0,0 +1,10 @@
+# ipak function: install and load multiple R packages.
+# check to see if packages are installed. Install them if they are not, then load them into the R session.
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
